6fe2f7e5a7833a3316bdd4c5de5a51b07e998a83
Cassie Souza <cassie.souza@harness.io>
2024-03-27 14:29:02 -0500

    Update data type

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,5 +1,4 @@
-output "vm_public_ip" {
-  description = "The public IP address of the virtual machine."
-  value       = azurerm_virtual_machine.example.network_interface_ids[0].private_ip_address
+output "vm_private_ip" {
+  description = "The private IP address of the virtual machine."
+  value       = azurerm_network_interface.example.private_ip_address
 }
-
